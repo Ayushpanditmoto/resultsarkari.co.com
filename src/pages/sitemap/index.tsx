a0c7e4fc5289439af886dff11925747f0644693b
Ayush Pandit <ayushpanditmoto@gmail.com>
2023-05-01 09:50:06 +0530

    fix

--- src/pages/sitemap/index.tsx
+++ src/pages/sitemap/index.tsx
@@ -1,20 +1,51 @@
 import React from 'react'
 import Link from 'next/link'
+import supabase from '@/config/Supabase.config'
+import { PostType } from '@/types/PostType'
+import PostContainer from '@/Layout/layout'
+import Center from '@/Layout/center'
 
-function Sitemap() {
+function Sitemap({ posts }: { posts: PostType[] }) {
   return (
     <div>
+      <Center>
+      <PostContainer>
       <h1>Sitemap</h1>
-
-      <ul>
-        <li><Link href="/">Home</Link></li>
-        <li><Link href="/about">About Page</Link></li>
-        <li><Link href="/sitemap">Sitemap</Link></li>
+      <ul style={{
+        padding : '2rem',
+      }}>
+        {posts.map((post) => (
+          <li style={{
+            
+            backgroundColor: '#0068ca',
+            margin: '1rem',
+            padding: '1rem',
+            borderRadius: '1rem',
+            color: '#fff',
+            fontSize: '1.5rem',
+          }} key={post.id}>
+            <Link href={`/${post.slug}`}>
+              {post.title}
+            </Link>
+          </li>
+        ))}
       </ul>
-
-
+      </PostContainer>
+      </Center>
     </div>
   )
 }
 
 export default Sitemap
+
+export async function getServerSideProps() {
+  const { data: posts, error } = await supabase
+  .from('posts')
+  .select('*')
+
+  return {
+    props: {
+      posts,
+    },
+  }
+}
