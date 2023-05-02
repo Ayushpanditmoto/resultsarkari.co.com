179a039a61d1ee7b8802b7697ac143bcd8b20d08
Ayush Pandit <ayushpanditmoto@gmail.com>
2023-05-03 00:41:33 +0530

    site map added

---
+++ src/pages/sitemap.xml.tsx
@@ -0,0 +1,48 @@
+import { GetServerSideProps } from "next";
+import supabase from "@/config/Supabase.config";
+
+
+export default function Sitemap() {
+  return null;
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+    res.setHeader("Content-Type", "text/xml");
+    const xml =await createSitemap();
+    res.write(xml);
+    res.end();
+    return {
+        props: {},
+        };
+    };
+
+
+    async function createSitemap() {
+        const { data: posts, error } = await supabase
+        .from('posts')
+        .select('*')
+        const createSiteMapUrls = async () => {
+            let postUrls = "";
+            posts!.map((post) => {
+                postUrls += `
+                <url>
+                <loc>https://resultsarkari.co.com/${post.slug}</loc>
+                <lastmod>${
+                    new Date(post.updated_at).toISOString().split("T")[0]
+                }</lastmod>
+                <priority>0.8</priority>
+                </url>
+                `;
+            });
+            
+            return postUrls;
+        };
+
+
+
+        return `<?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${await createSiteMapUrls()}</urlset>`;
+    }
+   
+
+
